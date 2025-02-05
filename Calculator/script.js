0f7900195d7f1be5ee31e88f14bd976eeec6ab1e
Salida Maharjan <salidamaharjan@outlook.com>
2025-02-05 10:54:29 -0600

    Display number when clicked

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,10 +1,93 @@
 let display = document.getElementsByClassName("numToDisplay")[0];
 
 function btn1Click() {
-    console.log(display.innerHTML);
+    // console.log(display.innerHTML);
     if(display.innerHTML == 0) {
         display.innerHTML = 1;
     } else {
         display.innerHTML = display.innerHTML + "1";
     }
+}
+function btn2Click() {
+    // console.log(display.innerHTML);
+    if(display.innerHTML == 0) {
+        display.innerHTML = 2;
+    } else {
+        display.innerHTML = display.innerHTML + "2";
+    }
+}
+function btn3Click() {
+    // console.log(display.innerHTML);
+    if(display.innerHTML == 0) {
+        display.innerHTML = 3;
+    } else {
+        display.innerHTML = display.innerHTML + "3";
+    }
+}
+
+function btn4Click() {
+    // console.log(display.innerHTML);
+    if(display.innerHTML == 0) {
+        display.innerHTML = 4;
+    } else {
+        display.innerHTML = display.innerHTML + "4";
+    }
+}
+
+function btn5Click() {
+    // console.log(display.innerHTML);
+    if(display.innerHTML == 0) {
+        display.innerHTML = 5;
+    } else {
+        display.innerHTML = display.innerHTML + "5";
+    }
+}
+
+function btn6Click() {
+    // console.log(display.innerHTML);
+    if(display.innerHTML == 0) {
+        display.innerHTML = 6;
+    } else {
+        display.innerHTML = display.innerHTML + "6";
+    }
+}
+
+function btn7Click() {
+    // console.log(display.innerHTML);
+    if(display.innerHTML == 0) {
+        display.innerHTML = 7;
+    } else {
+        display.innerHTML = display.innerHTML + "7";
+    }
+}
+
+function btn8Click() {
+    // console.log(display.innerHTML);
+    if(display.innerHTML == 0) {
+        display.innerHTML = 8;
+    } else {
+        display.innerHTML = display.innerHTML + "8";
+    }
+}
+function btn9Click() {
+    // console.log(display.innerHTML);
+    if(display.innerHTML == 0) {
+        display.innerHTML = 9;
+    } else {
+        display.innerHTML = display.innerHTML + "9";
+    }
+}
+function btn0Click() {
+    // console.log(display.innerHTML);
+    if(display.innerHTML == 0) {
+        display.innerHTML = 0;
+    } else {
+        display.innerHTML = display.innerHTML + "0";
+    }
+}
+function btnDotClick() {
+   
+}
+function btnEqlClick() {
+    
 }
