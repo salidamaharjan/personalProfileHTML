32aaf428c32df484bd95d94a71798feffd1bf5e1
Salida Maharjan <salidamaharjan@outlook.com>
2025-02-07 21:15:04 -0600

    Direct to the galleries page when clicked on ok

--- Topic1/personalProfile/js/script.js
+++ Topic1/personalProfile/js/script.js
@@ -1,3 +1,12 @@
+const placesVisited = document.getElementById("visited");
+
 function confirmRedirection(){
-    alert("Do you want to redirect to galleries?");
+   let result = confirm("Do you want to redirect to galleries?");
+   console.log(result);
+   if(result){
+    placesVisited.setAttribute("href", "./galleries.html#placesVisited");
+
+   } else {
+    placesVisited.setAttribute("href", "./hobbies.html");
+   }
 }
