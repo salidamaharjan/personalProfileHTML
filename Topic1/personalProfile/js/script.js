ad8b1f84cc43b89b6ec6abd226c991afdf6575a6
Salida Maharjan <salidamaharjan@outlook.com>
2025-02-07 22:34:27 -0600

    Add link to places detail

--- Topic1/personalProfile/js/script.js
+++ Topic1/personalProfile/js/script.js
@@ -1,6 +1,11 @@
 const placesVisited = document.getElementById("visited");
 const videosCreated = document.getElementById("videos");
 const pokharaD = document.getElementById("pokhara");
+const banffD = document.getElementById("banff");
+const aspenD = document.getElementById("aspen");
+const newYorkD = document.getElementById("newYork");
+const vegasD = document.getElementById("vegas");
+const mackinawD = document.getElementById("mackinaw");
 
 function redirectToPlaces(){
    let result = confirm("Do you want to go galleries to see places visited?");
@@ -22,4 +27,29 @@ function redirectToVideos(){
 function pokharaDetail() {
    pokharaD.setAttribute("href", "https://en.wikipedia.org/wiki/Pokhara");
    pokharaD.setAttribute("target", "_blank");
+}
+
+function banffDetail() {
+   banffD.setAttribute("href", "https://en.wikipedia.org/wiki/Banff,_Alberta");
+   banffD.setAttribute("target", "_blank");
+}
+
+function aspenDetail() {
+   aspenD.setAttribute("href", "https://en.wikipedia.org/wiki/Aspen,_Colorado");
+   aspenD.setAttribute("target", "_blank");
+}
+
+function newYorkDetail() {
+   newYorkD.setAttribute("href", "https://en.wikipedia.org/wiki/New_York_City");
+   newYorkD.setAttribute("target", "_blank");
+}
+
+function vegasDetail() {
+   vegasD.setAttribute("href", "https://en.wikipedia.org/wiki/Las_Vegas");
+   vegasD.setAttribute("target", "_blank");
+}
+
+function mackinawDetail() {
+   mackinawD.setAttribute("href", "https://en.wikipedia.org/wiki/Mackinac_Island");
+   mackinawD.setAttribute("target", "_blank");
 }
