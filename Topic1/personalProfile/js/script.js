b1b117dc7347ac3b43a252e8508fbfe5ed095050
Salida Maharjan <salidamaharjan@outlook.com>
2025-02-07 21:25:17 -0600

    Open galleies page when clicked on the ok button

--- Topic1/personalProfile/js/script.js
+++ Topic1/personalProfile/js/script.js
@@ -1,12 +1,19 @@
 const placesVisited = document.getElementById("visited");
+const videosCreated = document.getElementById("videos");
 
-function confirmRedirection(){
-   let result = confirm("Do you want to redirect to galleries?");
-   console.log(result);
+function redirectToPlaces(){
+   let result = confirm("Do you want to go galleries to see places visited?");
    if(result){
     placesVisited.setAttribute("href", "./galleries.html#placesVisited");
-
    } else {
     placesVisited.setAttribute("href", "./hobbies.html");
+   }
+}
+function redirectToVideos(){
+   let result = confirm("Do you want to go galleries to see videos created?");
+   if(result){
+    videosCreated.setAttribute("href", "./galleries.html#videosCreated");
+   } else {
+    videosCreated.setAttribute("href", "./hobbies.html");
    }
 }
